64fd4b1acf3a8048a31e4392a58768c4447742d4
johnmwangi <jonesmwas356@gmail.com>
2019-05-28 14:14:18 +0300

    profile changes

--- awards/views.py
+++ awards/views.py
@@ -88,7 +88,7 @@ def directory(request):
     profile =Profile.objects.get(username=current_user)
 
     winners=Project.objects.all()
-    caraousel=Project.objects.get(id=8)
+    caraousel=Project.objects.filter(id=8)
 
     return render(request,'directory.html',{"winners":winners,"profile":profile,"directory":directory,"caraousel":caraousel,"date":date})
 
